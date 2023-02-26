f25730dc853d8fae6f91a14f07e895cbf8c4c633
Arcade Wise <58434499+l3gacyb3ta@users.noreply.github.com>
2023-02-26 18:48:00 +0000

    Working?????????????

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,85 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { GetStaticProps, NextPage } from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
+// import { Disaster } from "../types/disaster";
+import disasters from "../data/naturaldisasters.json";
 
-const Home: NextPage = () => {
+type ListItem = {
+  name: string;
+  id: string;
+  img: string;
+}
+
+function hash(string: string): string {
+  //set variable hash as 0
+  var hash = 0;
+  // if the length of the string is 0, return 0
+  if (string.length == 0) return hash.toString();
+  for (var i = 0; i < string.length; i++) {
+    let ch = string.charCodeAt(i);
+    hash = ((hash << 5) - hash) + ch;
+    hash = hash & hash;
+  }
+
+  return hash.toString()
+}
+
+type HomeProps = {
+  disasters: ListItem[]
+}
+
+const List: NextPage<HomeProps> = ({ disasters }) => {
+  console.log(disasters)
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Next.js + TailwindCSS</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-4xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://replit.com/@ErikoXDev/Nextjs-TailwindCSS?v=1">
-            Next.js + Tailwind!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="/list"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://replit.com/site/hosting"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Your repl is deployed automatically on replit.com!
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://replit.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/replit.svg" alt="Replit Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+    <main className="flex w-full flex-1 flex-col items-center justify-center px-20">
+      <div className="grid md:gap-4 grid-cols-3 grid-rows-3">
+        {
+          disasters.map((disaster) => (
+            <Link key={hash(disaster.id)} href={"/disaster/" + disaster.id}>
+              <div className="relative w-80 h-80">
+                <h2 className="absolute bottom-3 left-3 text-white z-10 text-2xl">{disaster.name}</h2>
+                <div className="absolute inset-0 z-0">
+                  <div className="h-full w-full bg-gradient-to-b from-transparent from-transparent to-black bg-opacity-75 absolute top-0 z-10 rounded-lg"></div>
+             
+                  <div className="h-full w-full absolute top-0 card" style={{
+                    backgroundImage: `url("${disaster.img}")`
+                  }}></div>
+                </div>
+              </div>
+            </Link>
+          ))
+        }
+      </div>
+    </main>
   )
 }
 
-export default Home
+/*
+<div class="relative">
+  <h2 className="absolute bottom-0 left-0 text-white z-10">Your h2 element</h2>
+  <div className="absolute inset-0 z-0">
+    <div className="h-full w-full bg-gradient-to-b from-transparent to-black bg-opacity-75 absolute top-0"></div>
+    <div className="h-full w-full absolute top-0" style={{
+                      backgroundImage: `url("${disaster.img}")`
+                    }}></div>
+  </div>
+</div>
+*/
+
+export const getStaticProps: GetStaticProps = async () => {
+  let data: ListItem[] = disasters.map((item) => (
+    {
+      name: item.name,
+      id: item.id,
+      img: item.picture
+    }));
+
+  return {
+    props: {
+      disasters: data
+    }
+  }
+}
+
+
+export default List;
